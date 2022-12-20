b58e75264acd3ea819ba29a123b081ee95fad604
Kai <Kai@DESKTOP-HILNCBN>
2022-12-19 19:03:20 -0800

    Add project files.

---
+++ EntertainmentAggregatorUI/Data/AniListAPIAccess.cs
@@ -0,0 +1,191 @@
+﻿
+using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+using static EntertainmentAggregatorUI.Data.AniListContainer;
+
+namespace EntertainmentAggregatorUI.Data
+{
+    public class AniListAPIAccess
+    {
+        public static GraphQLHttpClient client = new GraphQLHttpClient("https://graphql.anilist.co/", new NewtonsoftJsonSerializer());
+
+        public static async Task<List<Medium>> getCurrentSeasonalAnime()
+        {
+            var now = DateTime.Now;
+            string season;
+            if (now.Month > 0 && now.Month < 4)
+            {
+                season = "WINTER";
+            }
+            else if (now.Month > 3 && now.Month < 7)
+            {
+                season = "SPRING";
+            }
+            else if (now.Month > 7 && now.Month < 10)
+            {
+                season = "SUMMER";
+            }
+            else
+            {
+                season = "FALL";
+            }
+
+            //Console.WriteLine($"{now.Year}, {now.Year.GetType()}");
+            Console.WriteLine(season);
+
+            var seasonalAnimePage1 = await doQLQuery(1, season, now.Year);
+            var seasonalAnimePage2 = await doQLQuery(2, season, now.Year);
+
+            var seasonalAnime = seasonalAnimePage1.Page.media;
+            seasonalAnime.AddRange(seasonalAnimePage2.Page.media);
+            return seasonalAnime;
+
+        }
+
+        //Add a different query to go by airing status; if it is within two seasons of the current show it there;
+        // will keep out long running series like one piece that don't need to be shown
+
+
+        public static async Task<Root> doQLQuery(int pageNum, string seasonofYear, int seasonYearNum)
+        {
+            var animeMediaRequest = new GraphQLRequest
+            {
+                Query = @"
+                query ($page: Int, $season: MediaSeason, $seasonYear: Int) { 
+                    Page(page:$page, perPage:50) {
+                        pageInfo{
+                          total
+                          currentPage
+                          lastPage
+                          hasNextPage
+                          perPage
+                        }
+                        media(season:$season, seasonYear:$seasonYear, type:ANIME, sort:SCORE_DESC){
+                          id
+                          meanScore
+                          isAdult
+                          averageScore
+                          episodes
+                          duration
+                          description(asHtml: false)
+                          title{
+                            english
+                            romaji
+                          }
+                          format
+                          studios(isMain: true) {
+                            nodes{
+                                name
+                            }
+                          }
+                          genres
+                          source
+                          externalLinks {
+                            site
+                            url
+                            icon
+                            color
+                          }
+                          coverImage {
+                            extraLarge
+                            large
+                            medium
+                            color
+                          }
+                          siteUrl
+                          nextAiringEpisode {
+                            airingAt
+                            episode
+                          }
+      
+                        }
+                      }
+            }",
+                Variables = new
+                {
+                    page = pageNum,
+                    season = seasonofYear,
+                    seasonYear = seasonYearNum
+
+                }
+            };
+
+            //Skip root class -> goes directly into data
+            var graphQLResponse = await client.SendQueryAsync<Root>(animeMediaRequest);
+            var seasonalAnime = graphQLResponse.Data;
+            return seasonalAnime;
+
+
+        }
+        
+        // Filter to remove adult, movies, and OVAs; keep normal TV and ONA anime
+        public static List<Medium> filterForMainSesaonal(List<Medium> toFilter)
+        {
+            List<Medium> filtered = new List<Medium>();
+            foreach (var anime in toFilter)
+            {
+                //Guard clause
+                if (anime.isAdult || anime.format == "MOVIE" || anime.format == "SPECIAL" || anime.format == "OVA" || anime.format == "MUSIC" || anime.meanScore == null)
+                {
+                    continue;
+                }
+
+                filtered.Add(anime);
+
+
+            }
+            return filtered;
+        }
+
+        public static int animeComparison(Medium one, Medium two)
+        {
+            if (one.meanScore < two.meanScore)
+            {
+                return -1;
+            }
+            else if (one.meanScore == two.meanScore)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public static List<ExternalLink> linkFilter(Medium anime)
+        {
+            var filtered = new List<ExternalLink>();
+            foreach (var link in anime.externalLinks)
+            {
+               if (link.site != "Twitter" && link.site != "YouTube" && link.site != "Official Site" && link.site != "Instagram"
+                            && link.site != "Bilibili TV" && link.site != "TikTok")
+                {
+                    filtered.Add(link);
+                }
+            }
+            return filtered;
+        }
+
+        public static string calcTimeUntil(Medium anime)
+        {
+            var now = DateTime.Now;
+            var nextEp = DateTimeOffset.FromUnixTimeSeconds(anime.nextAiringEpisode.airingAt);
+            var timeBetween = nextEp - now;
+            return $@"EP{anime.nextAiringEpisode.episode}: {timeBetween.Days} Days {timeBetween.Hours} Hours";
+
+
+        }
+        
+
+        public static string dateToDay(long timeStamp)
+        {
+            var datTimeOffSet = DateTimeOffset.FromUnixTimeSeconds(timeStamp);
+            DateTime date = datTimeOffSet.DateTime;
+            date = date.AddHours(-8);
+            return date.DayOfWeek.ToString();
+        }
+    }
+}
+
---
+++ EntertainmentAggregatorUI/Data/AniListContainer.cs
@@ -0,0 +1,157 @@
+﻿using Newtonsoft.Json;
+
+namespace EntertainmentAggregatorUI.Data
+{
+    public class AniListContainer
+    {
+        public class CoverImage
+        {
+            [JsonProperty("extraLarge")]
+            public string extraLarge { get; set; }
+
+            [JsonProperty("large")]
+            public string large { get; set; }
+
+            [JsonProperty("medium")]
+            public string medium { get; set; }
+
+            [JsonProperty("color")]
+            public string color { get; set; }
+        }
+
+        public class Root
+        {
+            [JsonProperty("Page")]
+            public Page Page { get; set; }
+        }
+
+        public class ExternalLink
+        {
+            [JsonProperty("site")]
+            public string site { get; set; }
+
+            [JsonProperty("url")]
+            public string url { get; set; }
+
+            [JsonProperty("icon")]
+            public string icon { get; set; }
+
+            [JsonProperty("color")]
+            public string color { get; set; }
+        }
+
+        public class Medium
+        {
+            [JsonProperty("id")]
+            public int id { get; set; }
+
+            [JsonProperty("meanScore")]
+            public Nullable<int> meanScore { get; set; }
+
+            [JsonProperty("averageScore")]
+            public Nullable<int> averageScore { get; set; }
+
+            [JsonProperty("isAdult")]
+            public bool isAdult { get; set; }
+
+            [JsonProperty("episodes")]
+            public int? episodes { get; set; }
+
+            [JsonProperty("duration")]
+            public int? duration { get; set; }
+
+            [JsonProperty("description")]
+            public string description { get; set; }
+
+            [JsonProperty("title")]
+            public Title title { get; set; }
+
+            [JsonProperty("format")]
+            public string format { get; set; }
+
+            [JsonProperty("studios")]
+            public Studios studios { get; set; }
+
+            [JsonProperty("genres")]
+            public List<string> genres { get; set; }
+
+            [JsonProperty("source")]
+            public string source { get; set; }
+
+            [JsonProperty("externalLinks")]
+            public List<ExternalLink> externalLinks { get; set; }
+
+            [JsonProperty("coverImage")]
+            public CoverImage coverImage { get; set; }
+
+            [JsonProperty("siteUrl")]
+            public string siteUrl { get; set; }
+
+            [JsonProperty("nextAiringEpisode")]
+            public NextAiringEpisode nextAiringEpisode { get; set; }
+        }
+
+        public class NextAiringEpisode
+        {
+            [JsonProperty("airingAt")]
+            public int airingAt { get; set; }
+
+            [JsonProperty("episode")]
+            public int episode { get; set; }
+        }
+
+        public class Node
+        {
+            [JsonProperty("name")]
+            public string name { get; set; }
+        }
+
+        public class Page
+        {
+            [JsonProperty("pageInfo")]
+            public PageInfo pageInfo { get; set; }
+
+            [JsonProperty("media")]
+            public List<Medium> media { get; set; }
+        }
+
+        public class PageInfo
+        {
+            [JsonProperty("total")]
+            public int total { get; set; }
+
+            [JsonProperty("currentPage")]
+            public int currentPage { get; set; }
+
+            [JsonProperty("lastPage")]
+            public int lastPage { get; set; }
+
+            [JsonProperty("hasNextPage")]
+            public bool hasNextPage { get; set; }
+
+            [JsonProperty("perPage")]
+            public int perPage { get; set; }
+        }
+        /*
+        public class Root
+        {
+            [JsonProperty("data")]
+            public Data data { get; set; }
+        }
+        */
+        public class Studios
+        {
+            [JsonProperty("nodes")]
+            public List<Node> nodes { get; set; }
+        }
+
+        public class Title
+        {
+            [JsonProperty("english")]
+            public string english { get; set; }
+
+            [JsonProperty("romaji")]
+            public string romaji { get; set; }
+        }
+    }
+}
